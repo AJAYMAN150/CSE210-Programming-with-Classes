86d9cc404ede3b828adb129a88c46e659e5d5d00
AJAYMAN <ajvaka20@gmail.com>
2024-10-24 01:35:39 -1000

    Learning 6

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+  List<Shape> shapes = new List<Shape>();
+
+        // Add different shapes to the list
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2));
+
+        // Iterate through the list and display color and area
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
